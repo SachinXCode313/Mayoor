eb4ff93593f2c1a51762becbdf4b58024763ba53
sg313 <sachin914597@gmail.com>
2025-02-01 22:40:35 +0530

    Adding Welcome msg to / route

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import bodyParser from "body-parser";
 import routers from './src/routes/routes.js';
 import dotenv from "dotenv"
-import db from './src/config/db.js';
 dotenv.config();
 
 const app = express();
@@ -14,13 +13,9 @@ app.use(bodyParser.json());
 
 app.use('/api',routers)
 
-app.get('/test', (req, res) => {
-    res.status(200).json({ message: "Server is working fine!" });
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Hey!!! Server is working fine!" });
 });
-
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
