4b9aa8727a255dcf8001052de235f13ed6ced201
sg313 <sachin914597@gmail.com>
2025-02-04 23:35:58 +0530

    adding notification feature

--- Backend/src/config/firebase-admin.js
+++ Backend/src/config/firebase-admin.js
@@ -1,26 +1,15 @@
 import admin from 'firebase-admin'
-// import { readFileSync } from 'fs'
-// import path from 'path'
-// import { fileURLToPath } from 'url'
-
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
 
 export const initializeFirebase = () => {
   try {
-    // const serviceAccount = JSON.parse(
-    //   readFileSync(path.resolve(__dirname, '../../firebase-adminsdk.json'), 'utf8')
-    // )
-    
-    const serviceAccount = {
-      type: process.env.FIREBASE_TYPE,
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    }
 
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert({
+        type: process.env.FIREBASE_TYPE,
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      }),
     })
 
     console.log("Firebase Admin SDK initialized successfully")
