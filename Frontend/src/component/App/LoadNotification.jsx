fe5ee2292815f7e08a33e80e83752a9aeb7e069c
sg313 <sachin914597@gmail.com>
2025-02-05 17:54:00 +0530

    fixing

--- Frontend/src/component/App/LoadNotification.jsx
+++ Frontend/src/component/App/LoadNotification.jsx
@@ -1,15 +1,27 @@
-// import React from 'react'
-// import { useEffect } from 'react';
-// import { onMessage } from 'firebase/messaging';
-// import { messaging} from '../../Helper/firebase';
-// import requestNotificationPermission from '../../Helper/push';
+import React from 'react'
+import { useEffect } from 'react';
+import { onMessage } from 'firebase/messaging';
+import { messaging} from '../../Helper/firebase';
+import requestNotificationPermission from '../../Helper/push';
 
 
-// const LoadNotification = () => {
+const LoadNotification = () => {
+    useEffect(() => {
+  requestNotificationPermission();
+
+  onMessage(messaging, (payload) => {
+    console.log("Received foreground message:", payload);
+    const { title, body } = payload.notification || {};
+
+    new Notification(title,{
+      body: body || "Foreground body",
+    })
+    
+  });
+}, []);
 
 
+  return
+}
 
-//   return
-// }
-
-// export default LoadNotification
+export default LoadNotification
--- Frontend/src/component/App/index.jsx
+++ Frontend/src/component/App/index.jsx
@@ -14,7 +14,7 @@ const App = () => {
     return (
       <>
         {/* <Login/> */}
-        
+        <LoadNotification/>
         {
           user ? <Home user={user} /> : <Login setUser = {setUser} />
         }
