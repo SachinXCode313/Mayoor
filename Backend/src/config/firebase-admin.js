c1aab7c0ffedbd4ae2309bb6003dc25f78808f3a
sg313 <sachin914597@gmail.com>
2025-02-06 08:58:06 +0530

    vercel deploy

--- Backend/src/config/firebase-admin.js
+++ Backend/src/config/firebase-admin.js
@@ -2,6 +2,19 @@ import admin from 'firebase-admin'
 
 export const initializeFirebase = () => {
   try {
+<<<<<<< HEAD
+=======
+    // const serviceAccount = JSON.parse(
+    //   readFileSync(path.resolve(__dirname, '../../firebase-adminsdk.json'), 'utf8')
+    // )
+    
+    const serviceAccount = {
+      type: process.env.FIREBASE_TYPE,
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }
+>>>>>>> 4e336ae (adding notification feature)
 
     admin.initializeApp({
       credential: admin.credential.cert({
