41804fe2dba3ca95b10e7b65dc332308b2fff6d3
sg313 <sachin914597@gmail.com>
2025-02-26 23:12:05 +0530

    cors policy updated

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://mayoor-flax.vercel.app" , "https://push-notification-murex-three.vercel.app"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://mayoor-flax.vercel.app" , "https://login-app-fawn.vercel.app"],
     methods: ['GET', 'POST', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', "subject", "year", "classname", "quarter", "section", "ac_id", "lo_id", "ro_id", "student_id"],
